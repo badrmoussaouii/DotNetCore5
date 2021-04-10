906d054b9eb3433da7972cc6a8b8fb12170bd738
badr moussaoui <badreddine_moussaoui@hotmail.com>
2021-04-10 20:30:27 +0100

    Ajoutez des fichiers projet.

---
+++ DotNetCore5/Migrations/20210410165116_RateFloat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetCore5.Migrations
+{
+    public partial class RateFloat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ DotNetCore5/ViewModel/MovieFormViewModel.cs
@@ -0,0 +1,37 @@
+﻿using DotNetCore5.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore5.ViewModel
+{
+    public class MovieFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(250)]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        
+        [Range(0, 10)]
+        public double Rate { get; set; }
+
+        [Required, MaxLength(2500)]
+        public string Storeline { get; set; }
+        
+        [Display(Name = "Select Poster")]
+        public byte[] Poster { get; set; }
+
+        [Display(Name = "Genre")]
+        public int GenreId { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
